eeb41a3322b7c21e8417df333b90ffe2ded54367
Nick Warms <nwarms96@gmail.com>
2023-12-01 15:52:58 +0000

    Add project files.

---
+++ AdventOfCode2024/FileReader.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2023
+{
+    public class FileReader
+    {
+
+        public string ReadFile(string filename)
+        {
+            try
+            {
+            StreamReader sr = new StreamReader("./Inputs/"+filename);
+            return sr.ReadToEnd();
+
+            } catch (FileNotFoundException ex)
+            {
+                return ex.Message;
+            }
+            
+        }
+    }
+}
