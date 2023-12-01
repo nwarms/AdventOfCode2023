d9e99338f60325bc840e56582bb79b7ea822b2df
Nick Warms <nwarms96@gmail.com>
2023-12-01 16:50:53 +0000

    Add method to read by file

--- AdventOfCode2023/FileReader.cs
+++ AdventOfCode2023/FileReader.cs
@@ -16,5 +16,24 @@
             }
             
         }
+
+        public List<string> ReadLines(string filename)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                StreamReader sr = new StreamReader("./Inputs/" + filename);
+                string line;
+                while ((line = sr.ReadLine()) is not null)
+                {
+                    lines.Add(line);
+                }
+
+            }
+            catch (FileNotFoundException ex)
+            {
+            }
+            return lines;
+        }
     }
 }
